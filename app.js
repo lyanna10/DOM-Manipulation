4c6405bfa50f21db777fd95340d8f8172487f42b
Lyann10 <glody.albadose@gmail.com>
2023-06-08 15:17:16 -0500

    DOM Manipulation Lab

---
+++ app.js
@@ -0,0 +1,46 @@
+// Select and cache the <main> element in a variable named mainEl
+const mainEl = document.querySelector("main");
+
+// Set the background color of mainEl to the value stored in the --main-bg CSS custom property
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+// Set the content of mainEl to <h1>DOM Manipulation</h1>
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+
+// Add a class of flex-ctr to mainEl
+mainEl.classList.add("flex-ctr");
+
+//Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+const topMenuEl = document.getElementById("top-menu");
+
+//Set the height topMenuEl element to be 100%
+topMenuEl.style.height = "100%";
+
+//Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+topMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+
+//Add a class of flex-around to topMenuEl
+topMenuEl.classList.add("flex-around");
+
+//Copy the following data structure to the top of app.js:
+// Menu data structure
+const menuLinks = [
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
+];
+
+menuLinks.forEach(function (link) {
+  // Create an <a> element
+  const newLink = document.createElement('a');
+
+  // Add an href attribute with its value set to the href property of the "link" object
+  newLink.href = link.href;
+
+  // Set the new element's content to the value of the text property of the "link" object
+  newLink.textContent = link.text;
+
+  // Append the new element to the topMenuEl element
+  topMenuEl.appendChild(newLink);
+});
